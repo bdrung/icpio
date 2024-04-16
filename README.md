6e8fe94b35969f4e2c7c58567aa86dce57c9b185
Benjamin Drung <bdrung@posteo.de>
2024-04-16 03:04:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+icpio
+=====
+
+icpio is a tool to manage initramfs CPIO files for the Linux kernel. The Linux
+kernel's
+[initramfs buffer format]([https://www.kernel.org/doc/html/latest/driver-api/early-userspace/buffer-format.html)
+is based around the `newc` or `crc` CPIO formats. Multiple CPIO archives can be
+concatenated and the last archive can be compressed. Different compression
+algorithms can be used depending on what support was compiled into the Linux
+kernel. icpio is tailored to initramfs CPIO files and will not gain support for
+other CPIO formats.
+
+As of now, icpio supports examining and listing the content of the initramfs CPIO.
+
+Usage examples
+--------------
+
+Examine the content of the initramfs CPIO on an Ubunu 24.04 system:
+
+```
+$ icpio --examine /boot/initrd.img
+0	cpio
+77312	cpio
+7286272	cpio
+85523968	zstd
+```
+
+This initramfs CPIO consists of three uncompressed cpio archives followed by a
+Zstandard-compressed cpio archive.
+
+List the content of the initramfs CPIO on an Ubunu 24.04 system:
+
+```
+$ icpio --examine /boot/initrd.img
+.
+kernel
+kernel/x86
+kernel/x86/microcode
+kernel/x86/microcode/AuthenticAMD.bin
+kernel
+kernel/x86
+kernel/x86/microcode
+kernel/x86/microcode/.enuineIntel.align.0123456789abc
+kernel/x86/microcode/GenuineIntel.bin
+.
+usr
+usr/lib
+usr/lib/firmware
+usr/lib/firmware/3com
+usr/lib/firmware/3com/typhoon.bin.zst
+[...]
+```
+
+The first cpio contains only the AMD microcode. The second cpio contains only
+the Intel microcode. The third cpio contains firmware files and kernel modules.
+
+Quick examples comparing
+------------------------
+
+Runtime comparison measured with `time` over five runs on different initramfs
+cpios:
+
+| System               | File                        | Size   | Enties | icpio  | lsinitramfs |
+| -------------------- | --------------------------- | ------ | ------ | ------ | ----------- |
+| AMD Ryzen 7 5700G    | initrd.img-6.5.0-27-generic | 102 MB |   3496 | 0.052s |     14.243s |
+| AMD Ryzen 7 5700G    | initrd.img-6.8.0-22-generic |  63 MB |   1934 | 0.042s |      7.239s |
+| Raspberry Pi Zero 2W | initrd.img-6.5.0-1012-raspi |  24 MB |   1537 | 0.647s |     57.235s |
+
+icpio is 88 to 274 times faster than lsinitramfs.
+
+Commands used:
+
+```
+icpio -t /boot/$file | wc -l
+time icpio -t /boot/$file > /dev/null
+time lsinitramfs /boot/$file > /dev/null
+```
---
+++ src/main.rs
@@ -0,0 +1,86 @@
+// Copyright (C) 2024, Benjamin Drung <bdrung@posteo.de>
+// SPDX-License-Identifier: ISC
+
+use std::fs::File;
+use std::process::ExitCode;
+
+use gumdrop::Options;
+
+use icpio::examine_cpio_content;
+use icpio::list_cpio_content;
+
+#[derive(Debug, Options)]
+struct MyOptions {
+    #[options(help = "List the offsets of the cpio archives and their compression.")]
+    examine: bool,
+
+    #[options(short = 't', help = "List the contents of the cpio archives.")]
+    list: bool,
+
+    #[options(help = "print help message")]
+    help: bool,
+
+    #[options(short = 'V', help = "print version number and exit")]
+    version: bool,
+
+    #[options(free)]
+    file: String,
+}
+
+fn main() -> ExitCode {
+    let opts = MyOptions::parse_args_default_or_exit();
+    let executable = std::env::args().next().unwrap();
+    if opts.version {
+        let name = std::option_env!("CARGO_PKG_NAME").unwrap();
+        let version = std::option_env!("CARGO_PKG_VERSION").unwrap();
+        println!("{} {}", name, version);
+        return ExitCode::SUCCESS;
+    }
+    if opts.file.is_empty() {
+        eprintln!("{}: missing required cpio file argument", executable);
+        return ExitCode::from(2);
+    }
+
+    let file = match File::open(&opts.file) {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!(
+                "{}: Error: Failed to open '{}': {}",
+                executable, opts.file, e
+            );
+            return ExitCode::FAILURE;
+        }
+    };
+
+    if opts.examine {
+        match examine_cpio_content(file) {
+            Ok(()) => {}
+            Err(e) => {
+                eprintln!(
+                    "{}: Error: Failed to examine content of '{}': {}",
+                    executable, opts.file, e
+                );
+                return ExitCode::FAILURE;
+            }
+        }
+    } else if opts.list {
+        match list_cpio_content(file) {
+            Ok(()) => {}
+            Err(e) => {
+                eprintln!(
+                    "{}: Error: Failed to list content of '{}': {}",
+                    executable, opts.file, e
+                );
+                return ExitCode::FAILURE;
+            }
+        }
+    } else {
+        eprintln!(
+            "{}: Error: Either --examine or --list must be specified!",
+            executable
+        );
+        return ExitCode::FAILURE;
+    };
+
+    ExitCode::SUCCESS
+}
